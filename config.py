36494c5ba7cffc5c6a911d64be108aecc2113d21
eli-jaffe <60531996+eli-jaffe@users.noreply.github.com>
2021-10-27 18:00:58 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Standard TCP/IP
+ssh_host = '128.122.34.205'
+# port 22 by defaul
+
+ssh_username = 'SSH_USERNAME'
+ssh_password = 'SSH_PASSWORD'
+
+database_name = 'YOUR_DATABASE_NAME'
+localhost = '127.0.0.1'
+# port 3306 by default
+
+database_username = 'YOUR_USERNAME'
+database_password = 'YOUR_PASSWORD'
